eb9523854ddb9e1e4410a7d53c712b1f34929f3b
Maksim <bobreczovm@inbox.ru>
2023-10-26 10:31:16 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp30/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int > list = new List<int>() { 1,2,3,4,5,6,7,8,9};
+            Stack<int> stack = new Stack<int>(list);
+
+            var value  = stack.Max();
+
+            var i = value.ToString(); ;
+            Console.WriteLine(i);
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp30/Stack.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp30
+{
+    public class Stack<T> : IComparable<T>
+    {
+        public List<T> Values { get ; set; } 
+
+        public Stack(List<T> values)
+        {
+            Values = values;
+        }
+
+        public object Max()
+        {
+            var MAX=    Values .Max();
+            Console.WriteLine(MAX);
+            return MAX;
+        }
+    }
+
+
+
+    public interface IComparable<T>
+    {
+        List<T> Values { get; set; }
+
+        object Max();
+      
+    }
+}
